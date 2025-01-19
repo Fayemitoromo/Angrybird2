68c076d6ced35645f772348994b0475290da8f01
Fayemitoromo <73675287+Fayemitoromo@users.noreply.github.com>
2025-01-19 18:39:12 -0500

    Created using Colab

---
+++ Lab1B.py
@@ -0,0 +1,58 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyM4Vk+WhvH5SdbWio/kDAke",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Fayemitoromo/Angrybird2/blob/main/Lab1B.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "D9g14N0D2MgS",
+        "outputId": "376b4874-bb33-43fb-d774-180cac5bcd15"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Enter a line of text: message\n",
+            "You wrote 'message'\n"
+          ]
+        }
+      ],
+      "source": [
+        "# Program Lab1B.py\n",
+        "# Demonstrate reading a string from the user.\n",
+        "# Reads a character string from the user and prints it.\n",
+        "message = input(\"Enter a line of text: \")\n",
+        "print(\"You wrote '\" + message + \"'\")\n"
+      ]
+    }
+  ]
+}
